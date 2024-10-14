14f8dbde88daa6505e49329bfbcea09bbd2bd0d3
vochik31531 <vova-mavrodi@mail.ru>
2024-10-14 19:14:54 +0300

    commit

--- modules/Users/Users.tsx
+++ modules/Users/Users.tsx
@@ -4,17 +4,20 @@ import MyTextField from "./ui/MyTextField";
 import MyTable from "./ui/MyTable";
 import UsersUI from "./ui/UsersUI";
 import { getUserGroups } from "./model/getUsersGroups";
+import { Suspense } from "react";
 
 export async function Users() {
   const users = await getUsers();
   const groups = getUserGroups(users);
 
   return (
-    <UsersUI
-      MyTextField={<MyTextField />}
-      MySelect={<MySelect items={groups} />}
-    >
-      <MyTable users={users} />
-    </UsersUI>
+    <Suspense>
+      <UsersUI
+        MyTextField={<MyTextField />}
+        MySelect={<MySelect items={groups} />}
+      >
+        <MyTable users={users} />
+      </UsersUI>
+    </Suspense>
   );
 }
