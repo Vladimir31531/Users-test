859bc68609fca52d18f643ee7a88073581177dc2
vochik31531 <vova-mavrodi@mail.ru>
2024-10-15 16:58:30 +0300

    add loader in button

--- modules/Users/model/handlersMyTable.ts
+++ modules/Users/model/handlersMyTable.ts
@@ -1,11 +1,16 @@
 import { changeUserStatus } from "@/lib/actions/user.action";
 import { IUser } from "@/types/users";
 
-export const handleOnOffUser = async (user: IUser) => {
+export const handleOnOffUser = async (
+  user: IUser,
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
+) => {
   try {
+    setIsLoading(true);
     const updatedUser = { ...user, active: !user.active };
 
     const res = await changeUserStatus(updatedUser);
+    setIsLoading(false);
   } catch (error) {
     console.log(error);
   }
